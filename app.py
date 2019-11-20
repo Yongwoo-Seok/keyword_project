e75430529a6bb88d1d7d640bc2556581eefacd97
Yongwoo-Seok <cyan0320@kaist.ac.kr>
2019-11-21 02:47:51 +0900

    플래텀 크롤링 완성
    키워드 입력 시 플래텀에서
    검색 후 마지막 페이지까지 뉴스기사 크롤링 하는 기능 업데이트

--- app.py
+++ app.py
@@ -5,8 +5,47 @@ from pymongo import MongoClient
 #client = MongoClient('localhost', 27017)
 #db = client.keyword_project
 
-# URL 받기
-url = 'https://www.venturesquare.net/?s=오쉐어'
+#플래텀에서 크롤링하기
+
+#keyword 받아서 url 만들기
+url1= 'https://platum.kr//?s='
+keyword = '스푼'
+url2 = url1 + keyword
+
+headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+data = requests.get(url2,headers=headers)
+soup = BeautifulSoup(data.text, 'html.parser')
+
+
+#print(data.status_code == 200)
+#title = soup.select_one('#post-131732 > div > div > div > div.post_header_title.two_third.last > h5 > a').text
+#title2 = soup.select('#post-129194 > div > div > div > div.post_header_title.two_third.last > h5 > a')
+#news = soup.select('#page_content_wrapper > div > div > div.sidebar_content > div > div > div > div > div')
+
+Page = 1
+while data.status_code == 200 :
+    url3 = 'https://platum.kr/page/'
+    keyword1 = '?s=' + keyword
+    url4 = url3 + str(Page) + keyword1
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36'}
+    data = requests.get(url4, headers=headers)
+    soup = BeautifulSoup(data.text, 'html.parser')
+    news = soup.select('#page_content_wrapper > div > div > div.sidebar_content > div > div > div > div > div')
+    for a in news:
+        title = a.select_one('h5')
+        if title != None :
+            #date = a.select_one('div > div > div.cf.listing-meta.meta.below > time').text.strip()
+            # image = a.select_one('div > a > img')
+            # desc = a.select_one('div > div > div.excerpt > p').text.strip()
+            news_data = {
+                'title': title.text,
+            }
+            print(news_data)
+    Page += 1
+
+'''
+# 벤처 스퀘어에서 크롤링하기
 
 #keyword 받아서 url 만들기
 url1= 'https://www.venturesquare.net/?s='
@@ -48,76 +87,10 @@ else :
             date = a.select_one('div > div > div.cf.listing-meta.meta.below > time').text.strip()
             # image = a.select_one('div > a > img')
             # desc = a.select_one('div > div > div.excerpt > p').text.strip()
-            data = {
+            news_data = {
                 'title': title,
                 'date': date
             }
-            print(data)
-
-
-'''
-for i in range(3):
-    print(type(i+1))
-
-news =  soup.select('body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post')
-for a in news:
-    title = a.select_one('div > div > a').text.strip()
-    date = a.select_one('div > div > div.cf.listing-meta.meta.below > time').text.strip()
-    #image = a.select_one('div > a > img')
-    #desc = a.select_one('div > div > div.excerpt > p').text.strip()
-    data = {
-        'title' : title,
-        'date' :date
-    }
-    print(data)
-'''
-'''
-해야할 일 
-1. 검색했을 때 끝 페이지 찾아서 크롤링하기
-- 페이지 리스트 길이에서 뒤에서 2번째로 확인
-- 
-- 페이지 길이 4일때 케이스 찾지못함
-2. 날짜 순으로 소팅하기 : 보여줄때 소팅해서 보여주면 되겠지..?
+            print(news_data)
 '''
 
-'''
-검색했을 때 
-https://www.venturesquare.net/?s=키워드
-
-페이지 수 
-https://www.venturesquare.net/page/1?s=키워드
-제목
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-794015.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news > div > div > a
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-786467.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news.tag-30717 > div > div > a
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-786032.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news.tag-33827 > div > div > a
-
-날짜
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-794015.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news > div > div > div.cf.listing-meta.meta.below > time
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-786467.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news.tag-30717 > div > div > div.cf.listing-meta.meta.below > time
-
-내용
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-794015.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news > div > div > div.excerpt > p
-
-이미지
-body > div.main-wrap > div.main.wrap.cf > div > div > div.posts-list.listing-alt > article.post-794015.post.type-post.status-publish.format-standard.has-post-thumbnail.category-news > div > a > img
-
-마지막 페이지
-투자유치
-body > div.main-wrap > div.main.wrap.cf > div > div > div.main-pagination > a:nth-child(5)
-
-전동킥보드
-body > div.main-wrap > div.main.wrap.cf > div > div > div.main-pagination > a:nth-child(3)
-
-맥시멈 페이지 찾기
-page_list = soup.select('body > div.main-wrap > div.main.wrap.cf > div > div > div.main-pagination > a')
-page_length = len(page_list)
-print(page_list)
-print(page_length)
-print(page_list[2].text)
-
-검색결과의 페이지 수
-url3 ='https://www.venturesquare.net/page/'
-page = '2'
-keyword1 = '?s=' +keyword
-url4 = url3 + page + keyword1
-'''
